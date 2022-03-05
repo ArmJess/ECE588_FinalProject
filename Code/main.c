899144ee6b04b3111a466b63881c6eda16cb31b5
Ian Taylor <Ian7aylor@gmail.com>
2022-03-05 02:33:38 -0800

    Update main.c
    Added argument for filename
    Added in core info for core1, core2, and core3

--- Code/main.c
+++ Code/main.c
@@ -3,12 +3,15 @@
 #include "signal.h"
 #include "cache.h"
 
-void main(){
+void main(int argc, char *argv[]){
 
     //get time needs to be added
     //input file -> someone can make this argv, argc when they get a chance instead of hardcoding path below
     char *path;
-    path = "input1.txt";
+    path = "argv[0]";
+   
+    
+    printf("Path: %s", argv[1]);
     int loop_length = inputLength(path); //use this as the number of times we need to run the full loop through all cores (this is the number of rows inside the input file)
     printf("number of input rows: %d\n", loop_length); //can take this out -> was just for double checking
     FILE *file = fopen(path, "r"); //opening the file to parse inputs below
@@ -58,11 +61,22 @@ void main(){
     currentStats.core0.curr_signal = signalCore0;
 
     //Core1
-
+    currentStats.core1.mem_location = arr_input[0].block_access; //get mem location
+    inputSignals(&signalCore1, &statusCore1, arr_input[0].processor, 1, &Cache1, arr_input[0].block_access, arr_input[0].access_type); //get status and B/P signal
+    currentStats.core1.prev_status = statusCore1;
+    currentStats.core1.curr_signal = signalCore1;
+    
     //Core2
-
+    currentStats.core1.mem_location = arr_input[0].block_access; //get mem location
+    inputSignals(&signalCore1, &statusCore1, arr_input[0].processor, 2, &Cache2, arr_input[0].block_access, arr_input[0].access_type); //get status and B/P signal
+    currentStats.core0.prev_status = statusCore2;
+    currentStats.core0.curr_signal = signalCore2;
+ 
     //Core3
-
+    currentStats.core1.mem_location = arr_input[0].block_access; //get mem location
+    inputSignals(&signalCore1, &statusCore1, arr_input[0].processor, 3, &Cache3, arr_input[0].block_access, arr_input[0].access_type); //get status and B/P signal
+    currentStats.core0.prev_status = statusCore3;
+    currentStats.core0.curr_signal = signalCore3;
 
 
     //2. Getting new status from protocol functions
@@ -70,31 +84,50 @@ void main(){
         //call protocol function and send in &currentStats nested structure (pointer in function) so that the protocol can know the status of this one mem location in all caches
         //outputs of the protocol function can go into the currentStats structure new_status column
         //example here is just saying that the output of Core0 protocol function is Modified
-        currentStats.core0.new_status = Modified;
-        newCore0 = currentStats.core0.new_status;
+    currentStats.core0.new_status = Modified;
+    newCore0 = currentStats.core0.new_status;
 
     //Core1
+    currentStats.core1.new_status = Modified;
+    newCore1 = currentStats.core1.new_status; 
     
     //Core2
-
+    currentStats.core2.new_status = Modified;
+    newCore2 = currentStats.core2.new_status;
+    
     //Core3
+    currentStats.core3.new_status = Modified;
+    newCore3 = currentStats.core3.new_status;
 
     //3. Inserting new status into the caches
     //Core0
     insert(&Cache0, arr_input[0].block_access, newCore0);
 
     //Core1
-
+    insert(&Cache1, arr_input[0].block_access, newCore1);
+    
     //Core2
-
+    insert(&Cache2, arr_input[0].block_access, newCore2);
+    
     //Core3
+    insert(&Cache3, arr_input[0].block_access, newCore3);
 
     //Testing outputs
-    
+        //Core 0 debug
         printf("Testing: cache contents for Core0:\n");
         printCacheContent(&Cache0); //just included for some verification debug
-
-
+        
+        //Core 1 debug
+        printf("Testing: cache contents for Core1:\n");
+        printCacheContent(&Cache1); //just included for some verification debug
+        
+        //Core 2 debug
+        printf("Testing: cache contents for Core2:\n");
+        printCacheContent(&Cache2); //just included for some verification debug
+        
+        //Core 3 debug
+        printf("Testing: cache contents for Core3:\n");
+        printCacheContent(&Cache3); //just included for some verification debug
     fclose(file);
     //end get time and report out time taken
     return;
